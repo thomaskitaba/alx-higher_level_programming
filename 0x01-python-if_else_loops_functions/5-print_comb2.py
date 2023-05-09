b7859933cf853b5ca5558c5a1ddee1ea0918c020
thomaskitaba <thomas.kitaba@gmail.com>
2023-05-09 16:48:40 +0300

    update

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for i in range(100):
     if i < 99:
-        print(f"{i:2d}, ")
+        print("{:02d},".fromat(i), end=' ')
     else:
-        print(f"{i:2d}")
+        print("{:02d}".fromat(i))
