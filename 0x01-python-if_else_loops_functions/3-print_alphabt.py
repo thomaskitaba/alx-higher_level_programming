fbee4ec9652284938573705be421e5370d8931fe
thomaskitaba <thomas.kitaba@gmail.com>
2023-05-09 14:34:11 +0300

    update

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 for ascii_num in range(97, 123):
-  if chr(ascii_num) != "e" and chr(ascii_num) != "q":
-    print(f"{chr(ascii_num)}", end = '')
-  else:
+    if chr(ascii_num) != "e" and chr(ascii_num) != "q":
+        print(f"{chr(ascii_num)}", end='')
