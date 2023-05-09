9b02474fa5c12169ad69774c6057b818e60a7145
thomaskitaba <thomas.kitaba@gmail.com>
2023-05-09 22:15:06 +0300

    update

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -19,33 +19,33 @@ return (NULL);
 new = malloc(sizeof(listint_t));
 if (new == NULL)
 {
-  return (NULL);
+return (NULL);
 }
 j = 0;
 new->n = number;
 while(current)
 {
-  Next = current->next;
-  if (number < current->n && j == 0)
-  {
-    new->next = *head;
-    *head = new; /*check latter*/
-    return (*head);
-  }
-  if (Next == NULL && number >= current->n)
-  {
-    new->next = NULL;
-    current->next = new;
-    break;
-  }
-  if (number >= current->n && number < Next->n)
-  {
-    current->next = new;
-    new->next = Next;
-    break;
-  }
-  j++;
-  current = Next;
+Next = current->next;
+if (number < current->n && j == 0)
+{
+  new->next = *head;
+  *head = new; /*check latter*/
+  return (*head);
+}
+if (Next == NULL && number >= current->n)
+{
+  new->next = NULL;
+  current->next = new;
+  break;
+}
+if (number >= current->n && number < Next->n)
+{
+  current->next = new;
+  new->next = Next;
+  break;
+}
+j++;
+current = Next;
 }
 return (*head);
 }
