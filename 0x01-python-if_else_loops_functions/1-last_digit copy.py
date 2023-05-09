f7dfebe2a594e20be3f8a4f166fe51095640354e
thomaskitaba <thomas.kitaba@gmail.com>
2023-05-09 16:11:11 +0300

    update

--- 0x01-python-if_else_loops_functions/1-last_digit copy.py
+++ 0x01-python-if_else_loops_functions/1-last_digit copy.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
+if number < 0:
+    number -= 1
 main_num = number // 10
 if main_num != 0:
     last_digit = number - 10 * main_num
