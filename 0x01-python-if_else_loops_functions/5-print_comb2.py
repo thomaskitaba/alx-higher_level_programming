7357896bd25eadc7f3e1bf4cd5fb9a485d9276fd
thomaskitaba <thomas.kitaba@gmail.com>
2023-05-09 22:47:19 +0300

    update

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-for number in range(0, 100):
-    if number == 99:
-        print("{}".format(number))
+for num in range(0, 100):
+    if num == 99:
+        print("{}".fromat(num))
     else:
-        print("{:02}".format(number), end=", ")
+        print("{:02}".fromat(num), end=', ')
