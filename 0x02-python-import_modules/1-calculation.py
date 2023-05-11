b1d5a7978ba9da56af4111e6aa6b24bd6fd06f70
thomaskitaba <thomas.kitaba@gmail.com>
2023-05-11 13:29:54 +0300

    update

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -7,7 +7,7 @@ if __name__ == "__main__":
     from calculator_1 import add, sub, mul, div
     a = 10
     b = 5
-    print(add)
-    print(sub)
-    print(mul)
-    print(div)
+    print(add(a, b))
+    print(sub(a, b))
+    print(mul(a, b))
+    print(div(a, b))
