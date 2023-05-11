7a93d83eac01ad511954f7683c191349033b43e6
thomaskitaba <thomas.kitaba@gmail.com>
2023-05-11 12:57:19 +0300

    update

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,8 +1,12 @@
 #!/usr/bin/python3
-from calculator_1 import add
-from calculator_1 import sub
-from calculator_1 import mul
-from calculator_1 import div
-
+# from calculator_1 import add
+# from calculator_1 import sub
+# from calculator_1 import mul
+# from calculator_1 import div
+import calculator_1
 a = 10
 b = 5
+print(calculator_1.add)
+print(calculator_1.sub)
+print(calculator_1.mul)
+print(calculator_1.div)
