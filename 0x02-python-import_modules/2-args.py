8b322d6c774ff95362f000beedcbe7bece00c5af
thomaskitaba <thomas.kitaba@gmail.com>
2023-05-11 14:15:44 +0300

    update

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
-import sys
 if __name__ == "__main__":
-    length = len(sys.argv)
+    import sys
+    length = len(sys.argv) - 1
     if length == 0:
         print("{} argument.".fromat(length))
     else:
-        for i in range(1, length):
+        for i in range(1, length + 1):
             print("{} arguments".format(length))
             print("{}: {}".format(sys.argv[i]))
