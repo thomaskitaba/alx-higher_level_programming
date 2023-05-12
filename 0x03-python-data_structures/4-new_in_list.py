5292d93d49368e8026b0adc2f43d7a61d3de0388
thomaskitaba <thomas.kitaba@gmail.com>
2023-05-12 12:34:42 +0300

    update

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def new_in_list(my_list, idx, element):
+  new_list = []
+  if idx < 0 or idx > len(my_list) - 1:
+    return (my_list)
+  for i in len(my_list):
+    new_list.append(my_list[i])
+
+  print("{}".format(my_list))
+  print("{}".format(new_list))
