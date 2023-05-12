439f25b234eef30fe29eb22ca432f4645e207cc4
thomaskitaba <thomas.kitaba@gmail.com>
2023-05-12 13:47:19 +0300

    update

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,8 @@
+#!/urs/bin/python3
+def print_matrix_integer(matrix=[[]]):
+  for arg in martrix:
+      for i in range(len(arg)):
+        if i == len(arg) - 1:
+          print("{}".format(arg[i]))
+        else:
+          print("{}".format(arg[i]), end=' ')
