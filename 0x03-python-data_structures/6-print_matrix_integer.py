6c1fbccd436b442e22af909601dd2f1e92a4c24d
thomaskitaba <thomas.kitaba@gmail.com>
2023-05-13 22:58:05 +0300

    update

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -3,7 +3,7 @@ def print_matrix_integer(matrix=[[]]):
     for arg in matrix:
         for i in range(len(arg)):
             if i == len(arg) - 1:
-                print("{:d}".format(arg[i]), end='')
+                print("{:d}".format(arg[i]), end="")
             else:
-                print("{:d}".format(arg[i]), end=' ')
-        print('$')
+                print("{:d}".format(arg[i]), end=" ")
+        print()
