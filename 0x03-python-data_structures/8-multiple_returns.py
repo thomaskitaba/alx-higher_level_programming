d80cfa55c09d941576bd7c112a040ad050b0b022
thomaskitaba <thomas.kitaba@gmail.com>
2023-05-13 22:14:23 +0300

    update

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    new_tuple = (len(sentence), sentence[0])
+    if not sentence:
+        new_tuple = (len(sentence), None)
+    else:
+        new_tuple = (len(sentence), sentence[0])
     return (new_tuple)
