bbad909583458e1c0f7eec29ea4e92595433c8e8
thomaskitaba <thomas.kitaba@gmail.com>
2023-05-14 11:30:49 +0300

    update

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,5 +1,31 @@
 #include "lists.h"
+/**
+*
+*
+*/
 
+listint_t *reverse_linked_list(listint_t *list)
+{
+listint_t *previous, *Next, *current;
+current = list;
+previous = Next = NULL;
+// set Next node
+while (current)
+{
+  Next = current->next;
+  // make the reverse
+  current->next = previous;
+  // adjust for next iteration
+  previous = current;
+  current = Next;
+}
+list = previous;
+return (list);
+}
+/**
+*
+*
+*/
 int is_palindrome(listint_t **head)
 {
 
