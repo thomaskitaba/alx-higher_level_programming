80fc9eb91d8911d18053a198913d55311270c50f
thomaskitaba <thomas.kitaba@gmail.com>
2023-05-14 19:16:48 +0300

    update

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -11,7 +11,7 @@ void print_python_list_info(PyObject *p)
 	PyObject *item;
 
 	/* to iterate*/
-	length = Py_SIZE;
+	length = Py_SIZE(p);
 	/* cast to PYlistobject pointer*/
 	allocated = (PyListObject *)p->allocated;
 	printf("[*] Size of the Python List = %d", length);
