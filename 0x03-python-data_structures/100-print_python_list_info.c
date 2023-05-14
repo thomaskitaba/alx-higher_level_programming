9f8134712cd6acdd9d93f8893aeec2733e7f7867
thomaskitaba <thomas.kitaba@gmail.com>
2023-05-14 17:08:04 +0300

    update

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -15,5 +15,4 @@ void print_python_list_info(PyObject *p)
         printf("index %d:  %s\n", i, Py_Type(Pylist_GetItem(p, i)->tp_name);
     }
     /*once we get size or length of the list we can traverse it*/
-    for ()
 }
