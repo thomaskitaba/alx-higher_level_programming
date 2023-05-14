76f28785a65ee0fc23c0dcdeca788c7d5deaa7aa
thomaskitaba <thomas.kitaba@gmail.com>
2023-05-14 13:57:09 +0300

    update

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -5,10 +5,10 @@
 * Return: head of reversed list
 */
 
-listint_t *reverse_linked_list(listint_t **list)
+listint_t *reverse_linked_list(listint_t **head)
 {
 listint_t *previous, *Next, *current;
-current = *list;
+current = *head;
 previous = Next = NULL;
 while (current)
 {
@@ -17,8 +17,8 @@ while (current)
 	previous = current;
 	current = Next;
 }
-*list = previous;
-return (*list);
+*head = previous;
+return (*head);
 }
 /**
 * is_palindrome - check if palindrome
@@ -31,16 +31,22 @@ listint_t *reversed_list, *current, *temp;
 current = *head;
 if (*head == NULL || (*head)->next == NULL)
 	return (1);
+/*while (p)
+{
+q = q->next;
+p = p->next->next;
+if (p == NULL)
+	reversed_list = reverse_linked_list(&(q->next->next));
+}*/
 reversed_list = reverse_linked_list(head);
 temp = reversed_list;
-
-while (!temp->next)
+while (!reversed_list->next)
 {
-	if (temp->n != current->n)
+	if (reversed_list->n != current->n)
 		return (0);
-	temp = temp->next;
+	reversed_list = reversed_list->next;
 	current = current->next;
 }
-*head = reverse_linked_list(&reversed_list);
+ reverse_linked_list(&temp);
 return (1);
 }
