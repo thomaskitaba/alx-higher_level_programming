b0574a69e812252b1d6f60a7e1d2797d3f481ad1
thomaskitaba <thomas.kitaba@gmail.com>
2023-05-14 17:01:50 +0300

    update

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -7,5 +7,16 @@
 */
 void print_python_list_info(PyObject *p)
 {
+    Py_ssize_t length, i
     printf("Hello Thomas Kitaba");
+
+
+    /* to itteraate*/
+    Py_ssize_t length = PyList_Size(list);
+    for (i = 0; i < lenght; i++)
+    {
+        printf("%s", PyType(Pylist_GetItem(p, i)->p_name);
+    }
+    /*once we get size or length of the list we can traverse it*/
+    for ()
 }
