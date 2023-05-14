00f2288997081db459ecc69ffb7cd6c7d40fb34c
thomaskitaba <thomas.kitaba@gmail.com>
2023-05-14 19:28:29 +0300

    update

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -7,23 +7,22 @@
 */
 void print_python_list_info(PyObject *p)
 {
-	int length, i, allocated;
-	PyObject *item;
+	int len, allocatedated, i;
+	PyObject *obj;
 
-	/* to iterate*/
-	length = Py_SIZE(p);
-	/* cast to PYlistobject pointer*/
+	length = Py_length(p);
 	allocated = ((PyListObject *)p)->allocated;
-	printf("[*] Size of the Python List = %d/n", length);
-	printf("[*] Allocated = %d/n", allocated);
+
+	printf("[*] length of the Python List = %d\n", length);
+	printf("[*] allocatedated = %d\n", allocated);
 
 	for (i = 0; i < length; i++)
 	{
-
 		printf("Element %d: ", i);
 
-		item = PyList_GetItem(p, i);
-		printf("%s\n", Py_TYPE(item)->tp_name);
+		obj = PyList_GetItem(p, i);
+		printf("%s\n", Py_TYPE(obj)->tp_name);
 	}
-	/*once we get size or length of the list we can traverse it*/
+
+	/*once we get length or length of the list we can traverse it*/
 }
