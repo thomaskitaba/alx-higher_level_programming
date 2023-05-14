132331a200303b6d7c4d14b05e5cda0c8d1d7e33
thomaskitaba <thomas.kitaba@gmail.com>
2023-05-14 17:22:51 +0300

    update

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -14,7 +14,7 @@ void print_python_list_info(PyObject *p)
 	length = PyList_Size(p);
 	for (i = 0; i < length; i++)
 	{
-		item = PyList_GetItem(p, i
+		item = PyList_GetItem(p, i);
 		printf("index %Zd:  %s\n", i, Py_TYPE(item)->tp_name));
 	}
 	/*once we get size or length of the list we can traverse it*/
