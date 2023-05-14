01a19b261ab347d2795626e8014ed8af522a258e
thomaskitaba <thomas.kitaba@gmail.com>
2023-05-14 16:02:19 +0300

    update

---
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <Python.h>
+/**
+* print_python_list_info - print basic list info
+* @p: pointer to a pythonlistobject
+* Return: nothing
+*/
+void print_python_list_info(PyObject *p);
+{
+    printf("Hello Thomas Kitaba");
+}
