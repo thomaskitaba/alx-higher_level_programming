2b8d7ed21a1e873858b3bcd17d509698c23c4b07
thomaskitaba <thomas.kitaba@gmail.com>
2023-05-17 19:40:40 +0300

    100-weight_average.py

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -1,1 +1,13 @@
 #!/usr/bin/python3
+def weight_average(my_list=[]):
+    if len(my_list) == 0:
+        return (0)
+    prod = 1
+    sum_of_prod = 0
+    total = 0
+    for data in my_list:
+        prod = data[0] * data[1]
+        sum_of_prod += prod
+        total += data[1]
+    # print("Average: {:0.2f}".format(sum_of_prod / total))
+    return (sum_of_prod / total)
