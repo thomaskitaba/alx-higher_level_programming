be0be558cee6b41201910fc0ef62d1c5c220e02f
thomaskitaba <thomas.kitaba@gmail.com>
2023-05-18 00:20:10 +0300

    12-python

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
+    if type(roman_string) != str or not roman_string:
+        return (0)
     d_r = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     roman_key = list(d_r.keys())
     sum = 0
@@ -15,4 +17,4 @@ def roman_to_int(roman_string):
                     sum -= d_r[roman_key[i]]
                 else:
                     sum += d_r[roman_key[i]]
-    return (sum)
+    return (int(sum))
