2e9878d6679ffa5e2203c084f8b54919898d1388
thomaskitaba <thomas.kitaba@gmail.com>
2023-05-17 16:15:45 +0300

    9-multiply by two

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -3,5 +3,6 @@ def multiply_by_2(a_dictionary):
     new_dict = {}
 
     for key, value in a_dictionary.items():
-        new_dict.update(key: value * 2)
+        new_value = value * 2
+        new_dict[key] =  new_value
     return (new_dict)
