b3d45006021c778c8d142146b5390220165b8ec8
thomaskitaba <thomas.kitaba@gmail.com>
2023-05-17 16:38:55 +0300

    10-best_score.py

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -3,6 +3,8 @@ def best_score(a_dictionary):
     if isinstance(a_dictionary, dict):
         if a_dictionary:
             value = list(a_dictionary.values())
+            if not value:
+                return (None)
             key = list(a_dictionary.keys())
             index_of_max = value.index(max(value))
         return (key[index_of_max])
