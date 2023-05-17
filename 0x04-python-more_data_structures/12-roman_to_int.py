e02283c133db99af2750dd8ee88320a86c65e24c
thomaskitaba <thomas.kitaba@gmail.com>
2023-05-17 22:57:07 +0300

    12-roman_to_int.py

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    dict_roman = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
-    roman_key = list(dict_roman.keys())
+    d_r = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    roman_key = list(d_r.keys())
     sum = 0
     for roman in roman_string:
         for i in range(len(roman_key)):
             if roman == roman_key[i]:
-                sum += dict_roman[roman_key[i]]
+                sum += d_r[roman_key[i]]
     print(sum)
+
 
 roman_to_int("II")
