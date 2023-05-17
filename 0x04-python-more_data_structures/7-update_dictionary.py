c449e693476c1859132f4cdd53c877b38ad19b1d
thomaskitaba <thomas.kitaba@gmail.com>
2023-05-17 15:35:13 +0300

    7-update_dictionary

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -5,4 +5,4 @@ def update_dictionary(a_dictionary, key, value):
     """
     if isinstance(a_dictionary, dict):
         a_dictionary.update({key : value})
-        return (a_dictionary)
+    return (a_dictionary)
--- 0x04-python-more_data_structures/8-simple_delete.py
+++ 0x04-python-more_data_structures/8-simple_delete.py
@@ -1,1 +1,5 @@
 #!/usr/bin/python3
+def simple_delete(a_dictionary, key=""):
+    if isinstance(a_dictionary, dict):
+        del a_dictionary[key]
+    return a_dictionary
