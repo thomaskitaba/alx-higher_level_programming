359955dbff596e87088980453eb652d257d8f060
thomaskitaba <thomas.kitaba@gmail.com>
2023-05-17 17:21:29 +0300

    102-complex_delete.py

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -4,12 +4,20 @@ def complex_delete(a_dictionary, value):
     list_key = list(a_dictionary.keys())
     list_value = list(a_dictionary.values())
 
-    try:
-        value_of_index = list_value.index(value)
-    except ValueError:
+    # try:
+    #     value_of_index = list_value.index(value)
+    # except ValueError:
+    #     return (a_dictionary)
+    found = 0
+    for i in range(len(list_value)):
+        if list_value[i] == value:
+            found = 1
+            value_of_index = i
+            break
+    if found == 0:
         return (a_dictionary)
-    key_to_delet = list_key[value_of_index]
 
+    key_to_delet = list_key[value_of_index]
     del a_dictionary[key_to_delet]
 
     return (a_dictionary)
