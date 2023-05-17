df83d0790341c41aa07a6fdb468cd8a25f003f92
thomaskitaba <thomas.kitaba@gmail.com>
2023-05-17 15:31:22 +0300

    7-update

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def update_dictionary(a_dictionary, key, value):
     if isinstance(a_dictionary, dict):
-        a_dictionary.update({key, value})
+        a_dictionary.update({key : value})
         return (a_dictionary)
