e6af5035999d33dbdb7043dba2261024af532737
thomaskitaba <thomas.kitaba@gmail.com>
2023-05-17 12:53:48 +0300

    2_search_replace.pu

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,1 +1,8 @@
 #!/usr/bin/python3
+def search_replace(my_list, search, replace):
+    result = []
+    for num in my_list:
+        if num == search:
+            num = replace
+        result.append(num)
+    reutrn (result)
