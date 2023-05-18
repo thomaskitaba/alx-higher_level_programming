48e22b6f79565a59e0827e1b8d590afdaab228c6
thomaskitaba <thomas.kitaba@gmail.com>
2023-05-18 11:02:46 +0300

    0-print dlistint

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -1,1 +1,17 @@
+#include "lists.h"
+
 size_t print_dlistint(const dlistint_t *h)
+{
+const dlistint_t *list;
+size_t i;
+
+list = h;
+i = 0;
+while(list)
+{
+    i++;
+    printf("%d", list->n);
+    list = list->next;
+}
+return (i);
+}
