dce14a2cf4cba334f220cfbb6be4c6f7f96fb4c7
thomaskitaba <thomas.kitaba@gmail.com>
2023-05-18 12:30:23 +0300

    5-get_dnodeint.c

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -1,1 +1,26 @@
+#include "lists.h"
+/**
+* get_dnodeint_at_index - get node at index
+* @head: head of the double linked list
+* @index: index of node to be searched
+* Return: pointer to the inserted node
+*/
 dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+dlistint_t *current;
+int i;
+current = head;
+i = 0;
+if (head == NULL)
+	return (NULL);
+while (current)
+{
+if (i == (int)index)
+{
+	return (current);
+}
+current = current->next;
+i++;
+}
+return (NULL);
+}
