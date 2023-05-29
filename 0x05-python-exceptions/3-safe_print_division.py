a0ed188517b904f36327bcf158a4ce949bff00ed
thomaskitaba <thomaskitaba@gmail.com>
2023-05-29 15:46:38 +0300

    update

---
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def safe_print_division(a, b):
+	result = 1
+	try:
+		result = a / b
+		return (result)
+	except ZeroDivisionError:
+		None
+	finally:
+		print("Inside result: ", end = '')
+		print("{}".format(result))
