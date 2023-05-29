e09d0b97893b8f316933c012274c4fa4f56dac6c
thomaskitaba <thomaskitaba@gmail.com>
2023-05-29 15:29:30 +0300

    update

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,20 +1,17 @@
 #!/usr/bin/python3
-def safe_print_list_integers((my_list=[], x=0):
-        count = 0
-        new_list = []
+def safe_print_list_integers(my_list=[], x=0):
+	count = 0
+	new_list = []
 
-        for row in my_list:
-            try:
-                if (my_list.index(row) <= x - 1):
-                    new_list.append(row)
-                    count += 1
-            except IndexError:
-                break
-        for new_row in new_list:
-            try:
-                print("{}".format(new_row), end='')
-            except (TypeError, ValueError):
-                break
-            finally:
-                print()
-        return (count)
+	for row in my_list:
+		try:
+			if (my_list.index(row) <= x - 1):
+				try:
+					print("{:d}".format(row), end='')
+					count += 1
+				except (TypeError, ValueError):
+					break
+		except IndexError:
+			break
+	print()
+	return (count)
