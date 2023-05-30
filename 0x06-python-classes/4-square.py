d95580a5099c2d30b96baa3d971e73156a3a2982
thomaskitaba <thomaskitaba@gmail.com>
2023-05-30 20:14:58 +0300

    update

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -7,10 +7,10 @@ class Square:
     def size(self):
         return (self.__size)
 
-    def size(self, size, value):
-        if type(size) not int:
+    def size(self, value):
+        if type(size) != int:
             raise TypeError("size must be an integer")
-        if type(size) is int and size < 0:
+        elif size < 0:
             raise ValueError("size must be >= 0")
         self.__ size = value
 
