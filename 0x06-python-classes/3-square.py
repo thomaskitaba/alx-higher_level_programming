d04b1a50d2e55f411604a9b8cbb0749e41368656
thomaskitaba <thomaskitaba@gmail.com>
2023-05-30 20:13:10 +0300

    update

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -5,7 +5,7 @@
 class Square:
     """attributes go here."""
     def __init__(self, size=0):
-        if type(size) != int:
+        if not isinstance(size, int):
             raise TypeError("size must be an integer")
         elif size < 0:
             raise ValueError("size must be >= 0")
