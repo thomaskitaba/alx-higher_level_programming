aeff4efaceef1327d5fdd606d4d0f77b7c261b5e
thomaskitaba <thomaskitaba@gmail.com>
2023-05-31 23:45:33 +0300

    update

--- 0x06-python-classes/100-singly_linked_list.py
+++ 0x06-python-classes/100-singly_linked_list.py
@@ -3,17 +3,16 @@
 
 
 class Node:
-    """ create Node """
+    """Create Node"""
 
     def __init__(self, data, next_node=None):
-
         self.data = data
         self.next_node = next_node
 
     @property
     def data(self):
-        """getter data"""
-        return (self.__data)
+        """Getter for data"""
+        return self.__data
 
     @data.setter
     def data(self, value):
@@ -23,47 +22,41 @@ class Node:
 
     @property
     def next_node(self):
-        """ setter for next node """
-        return (self.__next_node)
+        """Getter for next node"""
+        return self.__next_node
 
     @next_node.setter
     def next_node(self, value):
-        """setter next_node"""
+        """Setter for next node"""
         if not isinstance(value, Node) and value is not None:
             raise TypeError("next_node must be a Node object")
         self.__next_node = value
 
 
 class SinglyLinkedList:
-    """ create and mangage a singly linked list """
+    """Create and manage a singly linked list"""
+
     def __init__(self):
         self.__head = None
 
     def sorted_insert(self, value):
-        """ insert node on sorted position"""
+        """Insert node on sorted position"""
         new_node = Node(value)
-        """ if self.__head is None or value < self.__head.data:
-            new_node.next_node = self.__head
-            self.__head = new_node
-        """
         if self.__head is None or value < self.__head.data:
             new_node.next_node = self.__head
             self.__head = new_node
         else:
-            """ continue checking the other nodes """
             current = self.__head
-
-            while (current.next_node is not None and value > current.next_node.data):
+            while current.next_node is not None and value > current.next_node.data:
                 current = current.next_node
             new_node.next_node = current.next_node
             current.next_node = new_node
 
     def __str__(self):
-        """ print the linked list using print method """
+        """Print the linked list"""
         current = self.__head
         temp_list = []
-
         while current is not None:
             temp_list.append(str(current.data))
             current = current.next_node
-        return ('\n'.join(temp_list))
+        return "\n".join(temp_list)
