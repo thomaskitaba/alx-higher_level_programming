a639f0a5e9fdc96abd21ef244597c53331d3b3ec
thomaskitaba <thomaskitaba@gmail.com>
2023-05-31 22:51:28 +0300

    update

--- 0x06-python-classes/100-singly_linked_list copy.py
+++ 0x06-python-classes/100-singly_linked_list copy.py
@@ -3,7 +3,9 @@
 
 
 class Node:
+
     def __init__(self, data, next_node=None):
+
         self.data = data
         self.next_node = next_node
 
@@ -24,30 +26,36 @@ class Node:
     @next_node.setter
     def next_node(self, value):
         if value is not None and not isinstance(value, Node):
-            raise TypeError("next_node must be a Node or None")
+            raise TypeError("next_node must be a Node object")
         self.__next_node = value
 
 
 class SinglyLinkedList:
+
     def __init__(self):
         self.__head = None
 
     def sorted_insert(self, value):
+        """ insert node on sorted position"""
         new_node = Node(value)
         if self.__head is None or value < self.__head.data:
             new_node.next_node = self.__head
             self.__head = new_node
         else:
+            """ check the head first """
             current = self.__head
+
             while current.next_node is not None and value > current.next_node.data:
                 current = current.next_node
             new_node.next_node = current.next_node
             current.next_node = new_node
 
+
     def __str__(self):
         current = self.__head
         temp_list = []
+
         while current is not None:
             temp_list.append(str(current.data))
             current = current.next_node
-        return "\n".join(temp_list)
+        return ('\n'.join(temp_list))
