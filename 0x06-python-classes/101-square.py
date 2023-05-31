31bc0cae80895c588cc8d6b513c87fc7a6c6cd9a
thomaskitaba <thomaskitaba@gmail.com>
2023-05-31 13:35:36 +0300

    update

--- 0x06-python-classes/101-square.py
+++ 0x06-python-classes/101-square.py
@@ -6,7 +6,7 @@ class Square:
     """attributes go here."""
     def __init__(self, size=0, position=(0, 0)):
         self.size = size
-        self.__position = position
+        self.position = position
 
     @property
     def size(self):
