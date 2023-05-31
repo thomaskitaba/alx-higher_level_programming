15d7a0619a4a3d1b7bbbbfc0cabfbf51b1afb0b9
thomaskitaba <thomaskitaba@gmail.com>
2023-06-01 00:12:45 +0300

    last

--- 0x06-python-classes/101-square.py
+++ 0x06-python-classes/101-square.py
@@ -50,12 +50,16 @@ class Square:
             print()
 
     def __str__(self):
-        """print square using print method"""
-        if self.__size != 0:
-            [print("") for i in range(0, self.__position[1])]
-        for i in range(0, self.__size):
-            [print(" ", end="") for j in range(0, self.__position[0])]
-            [print("#", end="") for k in range(0, self.__size)]
-            if i != self.__size - 1:
-                print("")
-        return ("")
+        """ print instance of a class """
+        if self.__size == 0:
+            return ""
+        temp_str = ""
+        for j in range(0, self.__position[1]):
+            temp_str += ""
+        for row in range(self.__size):
+            for k in range(self.__position[0]):
+                temp_str += " "
+            for col in range(self.__size):
+                temp_str += "#"
+            temp_str += "\n"
+        return (temp_str)
