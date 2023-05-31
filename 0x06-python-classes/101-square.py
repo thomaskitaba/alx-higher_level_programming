cd459f36d28efaf0e0eda1a0cd481cae676fd0fa
thomaskitaba <thomaskitaba@gmail.com>
2023-05-31 13:27:35 +0300

    update

--- 0x06-python-classes/101-square.py
+++ 0x06-python-classes/101-square.py
@@ -31,7 +31,7 @@ class Square:
     def position(self, value):
         if not isinstance(value, tuple) or len(value) != 2 or \
           not all(isinstance(num, int) for num in value) or \
-          not all(num >=  0 for num in value):
+          not all(num >= 0 for num in value):
             raise TypeError("position must be a tuple of 2 positive integers")
 
         self.__position = value
@@ -48,17 +48,18 @@ class Square:
             for col in range(self.__size):
                 print("#", end='')
             print()
+
     def __str__ (self):
         """ print instance of a class """
         if self.__size == 0:
-            print()
-            return
-
+            return ""
+        temp_str = ""
         for j in range(0, self.__position[1]):
-            print()
+            temp_str += "\n"
         for row in range(self.__size):
             for k in range(self.__position[0]):
-                print(" ", end='')
+                temp_str += " "
             for col in range(self.__size):
-                print("#", end='')
-            print()
+                temp_str += "#"
+            temp_str += "\n"
+        return (temp_str)
