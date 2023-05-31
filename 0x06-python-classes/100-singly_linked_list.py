377ea0b60328084fcdaffca3ce0badafff7d1f11
thomaskitaba <thomaskitaba@gmail.com>
2023-05-31 18:37:04 +0300

    linked list using class

--- 0x06-python-classes/100-singly_linked_list.py
+++ 0x06-python-classes/100-singly_linked_list.py
@@ -10,23 +10,22 @@ class Node:
 
     @property
     def data(self):
-        return (self.__data)
+        return self.__data
 
     @data.setter
     def data(self, value):
-        if type(value) != int:
+        if isinstance(value, int):
             raise TypeError("data must be an integer")
-        else:
-            self.__data = value
+        self.__data = value
 
     @property
     def next_node(self):
-        return (self.__next_node)
+        return self.__next_node
 
-    @data.setter
+    @next_node.setter
     def next_node(self, value):
-        if type(value) != Node():  # check for node()
-            raise TypeError("data must be an integer")
+        if not isinstance(value, Node) and value is not None:
+            raise TypeError("next_node must be a Node object")
         else:
             self.__next_node = value
 
@@ -34,28 +33,36 @@ class Node:
 class SinglyLinkedList:
 
     def __init__(self):
-        self.head = None
+        self.__head = None
 
     def sorted_insert(self, value):
         """ insert node on sorted position"""
-        new_node = (value, head)
-
-        if self.head is None:
-            self.head = new_node
+        new_node = Node(value)
+        if self.__head is None:
+            self.__head = new_node
         else:
             """ check the head first """
-            current = self.head
-            Next = self.head
+            current = self.__head
+            Next = self.__head
             Next = Next.next_node
 
-            if value < current.data:
-                new_node.next_node = self.head
-                self.head = new_node
+            if value < self.__head.data:
+                new_node.next_node = self.__head
+                self.__head = new_node
 
-            while Next.next:
+            while Next.next_node:
                 if value < Next.data:
                     current.next_node = new_node
                     new_node.next_node = Next
                     return
             """ means it reaced the end of the list"""
             Next.next_node = new_node
+
+    def __str__(self):
+        current = self.__head
+        temp_list = []
+
+        while current is not None:
+            temp_list.append(str(current.data_))
+            current = current.next_node
+        return ('\n'.join(temp_list))
