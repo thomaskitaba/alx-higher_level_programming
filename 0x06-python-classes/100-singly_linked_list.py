6c37052a3bca56723168eccc35e460ac1e610a27
thomaskitaba <thomaskitaba@gmail.com>
2023-05-31 23:09:12 +0300

    update

--- 0x06-python-classes/100-singly_linked_list.py
+++ 0x06-python-classes/100-singly_linked_list.py
@@ -5,31 +5,31 @@
 class Node:
 
     def __init__(self, data, next_node=None):
-        """ instantiate Node """
+        """ inistanstiate"""
         self.data = data
         self.next_node = next_node
 
     @property
     def data(self):
-        """ getter for data """
-        return self.__data
+        """getter data"""
+        return (self.__data)
 
     @data.setter
     def data(self, value):
-        """ setter for data """
+        """ setter next_node """
         if not isinstance(value, int):
             raise TypeError("data must be an integer")
         self.__data = value
 
     @property
     def next_node(self):
-        """ getter for next_node """
-        return self.__next_node
+        """getter next_node"""
+        return (self.__next_node)
 
     @next_node.setter
     def next_node(self, value):
-        """ setter for next_node """
-        if value is not None and not isinstance(value, Node):
+        """ setter next_node """
+        if not isinstance(value, Node) and value is not None:
             raise TypeError("next_node must be a Node object")
         self.__next_node = value
 
