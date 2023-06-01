09c837b1cf9de0a07bcee6f6a8342b225056848f
thomaskitaba <thomaskitaba@gmail.com>
2023-06-01 16:24:00 +0300

    update

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+""" add integers """
+
+
+def add_integer(a, b=98):
+    """ function that adds two numbers """
+    result = 0
+    if (not isinstance(a, int) and not isinstance(a, float)):
+        raise TypeError("a must be an integer")
+    else:
+        a = int(a)
+    if (not isinstance(b, int) and not isinstance(b, float)):
+        raise TypeError("b must be an integer")
+    else:
+        b = int(b)
+    result = a + b
+    return (result)
+
+    """ if __name__ == "__main__":
+    doctest.testfile("0-add_integer.txt")
+    """
---
+++ 0x07-python-test_driven_development/test.py
@@ -0,0 +1,7 @@
+a = 3.5
+b = 4.9
+c = -0.5
+
+print("{} converted to {}".format(a, int(a)))
+print("{} converted to {}".format(b, int(b)))
+print("{} converted to {}".format(c, int(c)))
