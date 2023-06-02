386f0f6b300065056faf9d74add2ed22a2dafe8e
thomaskitaba <thomaskitaba@gmail.com>
2023-06-02 14:46:04 +0300

    update

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -1,10 +1,18 @@
 #!/usr/bin/python3
+
 def matrix_divided(matrix, div):
     length = 0
     new_matrix = []
     col = []
-    if not isinstance(matrix, list) and \
-      [not isinstance(row, list) for row in matrix]:
+    """ if not isinstance(matrix, list) and
+       not all(isinstance(row, list) for row in matrix):
+        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+    """
+    if div == 0:
+        raise ZeroDivisionError("division by zero")
+    if not isinstance(matrix, list):
+        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+    if not all(isinstance(row, list) for row in matrix):
         raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
     for row in matrix:
         for val in row:
@@ -18,19 +26,11 @@ def matrix_divided(matrix, div):
         if length != len(row):
             raise TypeError("Each row of the matrix must have the same size")
 
-    if div == 0:
-        raise ZeroDivisionError("division by zero")
-
     for row in matrix:
         col = []
         for val in row:
 
-            col.append(round(val/2, 2))
+            col.append(round(val/div, 2))
         if not None:
             new_matrix.append(col)
-    print(new_matrix)
-matrix = [
-    [1, 2, 3],
-    [4, 5, 6]
-]
-matrix_divided(matrix, 2)
+    return (new_matrix)
