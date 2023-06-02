f0b9d69da3edff42a33c495b72a15963b8873c67
thomaskitaba <thomaskitaba@gmail.com>
2023-06-02 16:41:34 +0300

    5-text_indentation

---
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+def text_indentation(text):
+    all_text = []
+    line_text = []
+
+    if not isinstance(text, str):
+        raise TypeError("text must be a string")
+
+    for i in range(len(text)):
+        line_text.append(text[i])
+        if text[i] == '.' or text[i] == '?' or text[i] == ':':
+            all_text.append([''.join(line_text)])
+            line_text.clear()
+
+    for row in all_text:
+        print(row[0].lstrip())
+        print('')
