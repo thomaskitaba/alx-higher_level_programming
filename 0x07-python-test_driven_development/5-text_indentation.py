386e532a1e146617604d174dcb02f8d3aeefc958
thomaskitaba <thomaskitaba@gmail.com>
2023-06-02 17:09:18 +0300

    5-text_indentaion update

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -6,12 +6,13 @@ def text_indentation(text):
     if not isinstance(text, str):
         raise TypeError("text must be a string")
 
+    """ iterate and create list of strings"""
     for i in range(len(text)):
         line_text.append(text[i])
         if text[i] == '.' or text[i] == '?' or text[i] == ':':
             all_text.append([''.join(line_text)])
             line_text.clear()
-
+    """ strip inital space for a string """
     for row in all_text:
         print(row[0].lstrip())
         print('')
