d24eea7657e45fc7b345a489451e50d6e3f8198f
thomaskitaba <thomaskitaba@gmail.com>
2023-06-03 21:19:43 +0300

    100-matrix_mul.txt update

--- 0x07-python-test_driven_development/100-matrix_mul.py
+++ 0x07-python-test_driven_development/100-matrix_mul.py
@@ -4,9 +4,9 @@ def matrix_mul(m_a, m_b):
         raise TypeError("m_a must be a list")
     if not isinstance(m_b, list):
         raise TypeError("m_b must be a list")
-    if len(m_a) == 0 or m_a is None or m_a[0] is None:
+    if len(m_a) == 0 or m_a == [] or any(len(row) == 0 for row in m_a):
         raise ValueError("m_a can't be empty")
-    if len(m_b) == 0 or m_b is None or m_b[0] is None:
+    if len(m_b) == 0 or m_b == [] or any(len(row) == 0 for row in m_b):
         raise ValueError("m_b can't be empty")
     for rows in m_a:
         if not all(isinstance(val, (int, float)) for val in rows):
@@ -19,7 +19,6 @@ def matrix_mul(m_a, m_b):
         if len(m_b) != len(rows):
             raise TypeError("each row of m_b must be of the same size")
 
-
     #TODO: If m_a and m_b can’t be multiplied: raise a ValueError exception with the message m_a and m_b can't be multiplied
 
     result = 0
@@ -28,13 +27,10 @@ def matrix_mul(m_a, m_b):
 
     for i in range (len(m_a)):
         c_value = []
-        for k in range(len(m_b[i])):
+        for k in range(len(m_a[i])):
             result = 0
-            for j in range(len(m_a[i])):
+            for j in range(len(m_b)):
                 result += m_a[i][j] * m_b[j][k]
             c_value.append(result)
         m_c.append(c_value)
-    print(m_c)
-a = [[1, 2], [3, 4]]
-b = [[1, 2], [3, 4]]
-matrix_mul(a, b)
+    return (m_c)
