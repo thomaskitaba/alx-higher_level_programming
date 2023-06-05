0575431fb5c11a7de6cc6347ffca90e0d003bd7a
thomaskitaba <thomaskitaba@gmail.com>
2023-06-05 13:09:21 +0300

    inception

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -5,5 +5,5 @@
 class Rectangle:
     """ def Rectangel with width and height """
     def __init__(self, width=0, height=0):
-        self.width = width
-        self.height = height
+        self.__width = width
+        self.__height = height
