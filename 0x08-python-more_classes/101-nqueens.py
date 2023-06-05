7e700ecb576bd373eabbceaa6fef15d8c8a9605a
thomaskitaba <thomaskitaba@gmail.com>
2023-06-05 20:15:25 +0300

    nqueens.py

---
+++ 0x08-python-more_classes/101-nqueens.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+import sys
+
+if __name__ == "__main__":
+    print(sys.argv)
+    """ check number of arguments """
+    N = 1
+    if len(sys.argv) != 2:
+        print("Usage: nqueens N")
+
+    """ check number of queens """
+    try:
+        N = int(sys.argv[1])
+        if int(sys.argv[1]) < 4:
+            print("N must be at least 4")
+            sys.exit(1)
+    except ValueError:
+        print("N must be a number")
+        sys.exit(1)
+    print('Create {} by {} board'.format(N, N))
+    # answer = set_queen(n)
