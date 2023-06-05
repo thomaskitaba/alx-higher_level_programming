6595145e34614a7d13c57d85e663f5941f059870
thomaskitaba <thomaskitaba@gmail.com>
2023-06-05 15:29:28 +0300

    _delet_a_rectangle_class_using_repr

--- 0x08-python-more_classes/6-rectangle.py
+++ 0x08-python-more_classes/6-rectangle.py
@@ -5,6 +5,7 @@
 class Rectangle:
     """ def Rectangel with width and height """
     number_of_instances = 0
+
     def __init__(self, width=0, height=0):
         self.width = width
         self.height = height
