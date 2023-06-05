c6e937ee0a8d6fa28d4651b67d1aec49f56cfcad
thomaskitaba <thomaskitaba@gmail.com>
2023-06-05 13:46:22 +0300

    inception

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -2,24 +2,32 @@
 """ class Rectangle that defines a rectangle """
 
 
-    class Rectangle:
+class Rectangle:
     """ def Rectangel with width and height """
     def __init__(self, width=0, height=0):
-        self.width = width
-        self.height = height
+        self.__width = width
+        self.__height = height
 
     @property
     def width(self):
-        return (self.width)
+        return (self.__width)
 
     @width.setter
     def width(self, value):
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
         self.__width = value
 
     @property
     def height(self):
-        return (self.height)
+        return (self.__height)
 
     @height.setter
-    def width(self, value):
+    def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
         self.__height = value
