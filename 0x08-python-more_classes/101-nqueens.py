daf2aafd1c1b5e6d5a37f4f24a6cfb262d82bea3
thomaskitaba <thomaskitaba@gmail.com>
2023-06-05 22:09:21 +0300

    nqueens.py

--- 0x08-python-more_classes/101-nqueens.py
+++ 0x08-python-more_classes/101-nqueens.py
@@ -2,57 +2,39 @@
 import sys
 
 def valid_VHD(ans, row, col):
-
     for row_ans in ans:
         # check horizontal
         if row_ans[0] == row or row_ans[1] == col:
-
             return False
-        # check if diagornal
+        # check if diagonal
         if abs(row - row_ans[0]) == abs(col - row_ans[1]):
-            is_diag = True
             return False
-
     return True
+
 def set_queen(N):
-    row = 0
-    col = 1
     ans = []
-    for y in range (1, N):
+    for y in range(N):
         location = [[0, y]]
-        ans = []
-        for row in range(0, N): # we can start checking from 1
-            if row == 0:
-                ans.append(location[0])
-                continue
-            for col in range(0, N):
-                # check if Vertical
-                if row == 2 and col == 0:
-                    continue
-                if valid_VHD(ans, row, col):
-                    ans.append([row, col])
+        ans = [location[0]]
+        for row in range(1, N):
+            for col_inner in range(N):
+                if valid_VHD(ans, row, col_inner):
+                    ans.append([row, col_inner])
                     break
         print(ans)
 
-                # check if Horizontal
-                # check if diagonal
-
-                #if not dieagonal H  or V append to answer
-
 if __name__ == "__main__":
-    """ check number of arguments """
     N = 1
     if len(sys.argv) != 2:
         print("Usage: nqueens N")
         sys.exit(1)
-    """ check number of queens """
     try:
         N = int(sys.argv[1])
-        if int(sys.argv[1]) < 4:
+        if N < 4:
             print("N must be at least 4")
             sys.exit(1)
     except ValueError:
         print("N must be a number")
         sys.exit(1)
 
-    answer = set_queen(N)
+    set_queen(N)
