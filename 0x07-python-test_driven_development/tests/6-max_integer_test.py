cf9d415bf21e6aa57f572165a25e58d1105331e2
thomaskitaba <thomaskitaba@gmail.com>
2023-06-07 20:04:19 +0300

    correction to unittest

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -60,10 +60,10 @@ class TestMaxInteger(unittest.TestCase):
         self.assertEqual(max_integer(test_list), 3.5)
     """ ---------------------------------------------- """
 
-    def test_string_list(self):
-        """ test list with strings """
-        list_string = ["abebe", "kebede", "feyissa", "zegeye"]
-        self.assertEqual(max_integer(list_string), "zegeye")
+    def test_empty_string(self):
+        """ test for empty string """
+        empty_string = ""
+        self.assertEqual(max_integer(empty_string), None)
 
 if __name__ == '__main__':
     unittest.main()
