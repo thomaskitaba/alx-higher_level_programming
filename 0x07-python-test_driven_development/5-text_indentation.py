f3dbdcd32117b068dcf149808ce3f1a8d71f0f9d
thomaskitaba <thomaskitaba@gmail.com>
2023-06-07 14:18:52 +0300

    correction to text_indentation

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -8,12 +8,15 @@ Parameters:
 
 def text_indentation(text):
     """ indent text when . : or ? is found """
+
     all_text = []
     line_text = []
 
     if not isinstance(text, str):
         raise TypeError("text must be a string")
 
+    if text is None:
+        raise ValueError("text must be a string")
     """ iterate and create list of strings"""
     for i in range(len(text)):
         line_text.append(text[i])
