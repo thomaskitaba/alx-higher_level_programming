0af90bfc77e0e48eb40a3b5a4c01cb6105f5adf0
thomaskitaba <thomaskitaba@gmail.com>
2023-06-07 20:41:45 +0300

    update

--- 0x07-python-test_driven_development/0-add_integer copy.py
+++ 0x07-python-test_driven_development/0-add_integer copy.py
@@ -9,11 +9,23 @@ Parameters:
 def add_integer(a, b=98):
     """ function that adds two numbers """
     result = 0
-
+    if a is None:
+        raise Exception("a must be an integer")
+    if a is None and b is None:
+        raise TypeError("a must be an integer")
     if (not isinstance(a, int) and not isinstance(a, float)):
         raise TypeError("a must be an integer")
+    else:
+        try:
+            a = int(a)
+        except Exception as e:
+            raise OverflowError("float Overflow") from e
     if (not isinstance(b, int) and not isinstance(b, float)):
         raise TypeError("b must be an integer")
-
-    result = int(a) + int(b)
+    else:
+        try:
+            b = int(b)
+        except Exception as e:
+            raise OverflowError("float Overflow") from e
+    result = a + b
     return (result)
