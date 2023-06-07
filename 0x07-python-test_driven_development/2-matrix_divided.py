e23aaa9980f465ddc56cca2b88c9dd93b4f23dc2
thomaskitaba <thomaskitaba@gmail.com>
2023-06-07 16:47:25 +0300

    correction to matrix division.py

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -7,32 +7,36 @@ Parameters:
 
 
 def matrix_divided(matrix, div):
-    """ divides all elements of a matrix. """
+    """
+    Divides all elements of a matrix.
+    """
 
     length = 0
     new_matrix = []
     col = []
-    """ if not isinstance(matrix, list) and
-       not all(isinstance(row, list) for row in matrix):
-        raise TypeError("matrix must be a matrix
-        (list of lists) of integers/floats")
-    """
+
+    if not isinstance(matrix, list) and not all(isinstance(row, list) for row in matrix):
+        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
     if div == 0:
         raise ZeroDivisionError("division by zero")
     if not isinstance(div, (int, float)):
         raise TypeError("div must be a number")
+
     if not isinstance(matrix, list):
         raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
     if not all(isinstance(row, list) for row in matrix):
         raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+
     for row in matrix:
         for val in row:
             if not isinstance(val, (int, float)):
                 raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+
     if matrix is not None:
         length = len(matrix[0])
     else:
         return matrix
+
     for row in matrix:
         if length != len(row):
             raise TypeError("Each row of the matrix must have the same size")
@@ -40,8 +44,7 @@ def matrix_divided(matrix, div):
     for row in matrix:
         col = []
         for val in row:
+            col.append(round(val / div, 2))
+        new_matrix.append(col)
 
-            col.append(round(val/div, 2))
-        if not None:
-            new_matrix.append(col)
-    return (new_matrix)
+    return new_matrix
