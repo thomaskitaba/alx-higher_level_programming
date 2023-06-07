8af2ba6975327b86ad7d2ce5df0cdf1db872dd72
thomaskitaba <thomaskitaba@gmail.com>
2023-06-07 12:47:41 +0300

    correction to 100-matrix mul

--- 0x07-python-test_driven_development/100-matrix_mul.py
+++ 0x07-python-test_driven_development/100-matrix_mul.py
@@ -14,6 +14,11 @@ def matrix_mul(m_a, m_b):
         raise TypeError("m_b must be a list")
     if len(m_a) == 0 or m_a == [] or any(len(row) == 0 for row in m_a):
         raise ValueError("m_a can't be empty")
+
+    if not all(isinstance(row, list) for row in m_a):
+        raise TypeError("m_a must be a list of lists")
+    if not all(isinstance(row, list) for row in m_b)
+        raise TypeError("m_b must be a list of lists")
     if len(m_b) == 0 or m_b == [] or any(len(row) == 0 for row in m_b):
         raise ValueError("m_b can't be empty")
     len_ma_row = len(m_a[0])
