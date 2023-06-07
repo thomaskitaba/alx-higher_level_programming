eb5d81fa106fb37291c2b300f72ab6f6c096d087
thomaskitaba <thomaskitaba@gmail.com>
2023-06-07 16:22:08 +0300

    correction_0_add_integer

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -16,12 +16,15 @@ def add_integer(a, b=98):
     if (not isinstance(a, int) and not isinstance(a, float)):
         raise TypeError("a must be an integer")
     else:
-        a = int(a)
+        try:
+            a = int(a)
+        except Exception as e:
+            raise OverflowError("cannot convert float infinity to integer") from e
     if (not isinstance(b, int) and not isinstance(b, float)):
         raise TypeError("b must be an integer")
     else:
         b = int(b)
     result = a + b
     if result > float('inf') or result < float('-inf'):
-        raise OverflowError("Overflow Occured")
+        raise OverflowError("cannot convert float infinity to integer")
     return (result)
