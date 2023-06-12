6becd44ba6ab3295f2f9e08129a299dffd1a0ebd
thomaskitaba <thomaskitaba@gmail.com>
2023-06-12 21:07:48 +0300

    10-square.py

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -4,3 +4,16 @@
 """
 
 BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Square(Rectangle):
+    """ create square instances """
+
+    def __init__(self, size):
+        """ instantiate size """
+        self.integer_validator("size", size)
+        self.__size = size
+
+    def area(self):
+        """ calculate area """
+        retrun (self.__size * self.__size)
