a450fee810ccb0ad091b3205def5980797583f29
thomaskitaba <thomaskitaba@gmail.com>
2023-06-12 16:39:58 +0300

    2-is_sme_class

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -3,7 +3,9 @@
 return True or False based on the object type
 """
 
+
 def is_same_class(obj, a_class):
-	if isinstance(obj, a_class):
-		return (True)
-	return (False)
+    """ check instance of obj """
+    if isinstance(obj, a_class):
+        return (True)
+    return (False)
