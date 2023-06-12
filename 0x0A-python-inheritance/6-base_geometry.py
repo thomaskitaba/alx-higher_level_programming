926941de6e683a0c9f3dcc0732fbed286097efbc
thomaskitaba <thomaskitaba@gmail.com>
2023-06-12 19:06:38 +0300

    BaseGeometry_area_added

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+Create an empty class called BaseGeometry
+"""
+
+
+class BaseGeometry:
+    """
+        this is an empty class
+    """
+    def area(self):
+        raise Exception as e("area() is not implemented")
