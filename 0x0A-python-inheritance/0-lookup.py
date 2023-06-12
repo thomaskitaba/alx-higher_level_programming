f941ad29e5f7f25ecb13d69eb13a83a461a8489b
thomaskitaba <thomaskitaba@gmail.com>
2023-06-12 14:27:47 +0300

    update

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -7,4 +7,4 @@ return list of available attributes and methods of an object
 def lookup(obj):
     """ check for obj and return desired result """
 	if (isinstance(obj, type) and hasattr(obj, "__dict__")):
-		return (dir(obj))
+		return ([dir(obj)])
