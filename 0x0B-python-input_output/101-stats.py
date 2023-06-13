abc309bb9d8ef9f9f6a51d75294f347dd4cff11d
thomaskitaba <thomaskitaba@gmail.com>
2023-06-13 23:58:56 +0300

    101-stats.py

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -9,36 +9,45 @@ def print_status(status):
 
 
 if __name__ == "__main__":
-
     line_count = 0
     size = 0
-    status = {"File size": 0,
-              "200": 0,
-              "301": 0,
-              "400": 0,
-              "401": 0,
-              "403": 0,
-              "404": 0,
-              "405": 0,
-              "500": 0}
-    possible_codes = ["200", "301", "400", "401", "403", "404", "405", "500"]
+    status = {
+        "File size": 0,
+        "200": 0,
+        "301": 0,
+        "400": 0,
+        "401": 0,
+        "403": 0,
+        "404": 0,
+        "405": 0,
+        "500": 0
+    }
+    possible_codes = {"200", "301", "400", "401", "403", "404", "405", "500"}
+
     try:
         while True:
-
             for line in sys.stdin:
                 line = line.split()
                 line_count += 1
+
                 if line_count == 10:
                     print_status(status)
                     line_count = 1
 
-                if int(line[-1]) > 0:
-                    status["File size"] += int(line[-1])
+                if len(line) > 0 and line[-1].isdigit():
+                    try:
+                        size = int(line[-1])
+                        status["File size"] += size
+                    except Exception:
+                        pass
+                try:
+                    if len(line) > 1 and line[-2] in possible_codes:
+                        status[line[-2]] += 1
+                except IndexError:
+                    pass
 
-                for code in possible_codes:
-                    if code == line[-2]:
-                        status[code] += 1
         print_status(status)
+
     except KeyboardInterrupt:
         print_status(status)
         raise
