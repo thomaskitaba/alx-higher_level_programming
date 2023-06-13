095f60e8a032127ae6e921a7dbd72b0df341cb92
thomaskitaba <thomaskitaba@gmail.com>
2023-06-13 18:51:37 +0300

    update

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -12,15 +12,14 @@ args = sys.argv[1:]
 """ load json file convert
     json string to python object
 """
-f = open("add_item.json", 'w')
-f.close()
-
+with open("add_item.json", 'w') as f:
+    f.write("")
 loaded_string = load_from_json_file("add_item.json")
 
 """ add arguments to the loaded
     python object
 """
-print(loaded_string)
+
 args += loaded_string
 
 """
