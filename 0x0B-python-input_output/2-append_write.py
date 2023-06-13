f8757e1d21a1b1bf517bc8a089fcde12244f7f02
thomaskitaba <thomaskitaba@gmail.com>
2023-06-13 15:24:52 +0300

    update

---
+++ 0x0B-python-input_output/2-append_write.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+    Write a function that writes
+    a string to a text file
+"""
+
+
+def append_write(filename="", text=""):
+    """
+    step-1 open the file in read mode
+    step-2 Read from the opend file
+    step-3 print it on the stdout
+    """
+
+    with open(filename, 'a') as f:
+        return f.write(text)
