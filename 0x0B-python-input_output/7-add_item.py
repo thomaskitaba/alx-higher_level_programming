0c0e6bf78d5bdb138b35aae6ccaaf25488750692
thomaskitaba <thomaskitaba@gmail.com>
2023-06-13 16:56:20 +0300

    7-add_item.py

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -7,16 +7,25 @@ import sys
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
-def load():
-    pass
-
-def add():
-    pass
-
-def save():
-    pass
-# if __name__ == "__main__":
 args = sys.argv[1:]
-file_name = "add_item.json"
+
+""" load json file convert
+    json string to python object
+"""
+
+loaded_string = load_from_json_file("add_item.json")
+
+""" add arguments to the loaded
+    python object
+"""
+
+if len(loaded_string) > 0:
+    args += loaded_string
+
+"""
+    save the python object to
+    a json file
+"""
+
 json_string = save_to_json_file(args, "add_item.json")
-print(type(json_string))
+# print(type(json_string))
