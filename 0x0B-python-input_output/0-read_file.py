afb432b6cd1b271042c6614b039419e2806fd042
thomaskitaba <thomaskitaba@gmail.com>
2023-06-13 14:30:05 +0300

    printfile

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+    read text file and print to stdout
+"""
+
+
+def read_file(filename=""):
+    """
+    step-1 open the file in read mode
+    step-2 Read from the opend file
+    step-3 print it on the stdout
+    """
+    with open(filename, 'r') as f:
+        my_file = f.read()
+        print(my_file)
