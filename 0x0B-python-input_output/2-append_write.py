7a2408d65a7a8aa0b77a016003a45595fb42404b
thomaskitaba <thomaskitaba@gmail.com>
2023-06-13 15:25:36 +0300

    2-append

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -9,7 +9,7 @@ def append_write(filename="", text=""):
     """
     step-1 open the file in read mode
     step-2 Read from the opend file
-    step-3 print it on the stdout
+    step-3 return number of appended char
     """
 
     with open(filename, 'a') as f:
