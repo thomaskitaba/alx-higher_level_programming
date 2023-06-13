59d04e7f5d368bc2f814de203fe80646e53b2776
thomaskitaba <thomaskitaba@gmail.com>
2023-06-13 23:20:24 +0300

    101_stdin_print.py

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -1,56 +1,22 @@
 #!/usr/bin/python3
 """ Read from standard input """
 import sys
+def print_status(status)
+    for key, value in status.items():
+        print("{}: {}".format(key, value))
 
 
-try:
-    while True:
+if __name__ == "__main__":
 
-        size_count = 0
-        line_count = 0
-        count_200 = 0
-        count_301 = 0
-        count_400 = 0
-        count_401 = 0
-        count_403 = 0
-        count_404 = 0
-        count_405 = 0
-        count_500 = 0
+    status = {"File size": 0, "200": 0, "301": 0, "400": 0, "401": 0, "403": 0, "404": 0, "405": 0, "500": 0}
+    possible_codes = ["200", "301", "400", "401", "403", "404", "405", "500"]
+    try:
+        while True:
 
-        status = ""
-        content = ""
-        for line in sys.stdin:
-            size_count = 0
-            if '200' in line:
-                count_200 += 1
-            if '301' in line:
-                count_301 += 1
-            if '400' in line:
-                count_400 += 1
-            if '401' in line:
-                count_401 += 1
-            if '403' in line:
-                count_403 += 1
-            if '404' in line:
-                count_404 += 1
-            if '405' in line:
-                count_405 += 1
-            if '500' in line:
-                count_500 += 1
+            for line in syt.stdin:
+                line = line.split()
 
-            size_count += len(line)
-            line_count += 1
-            status = 'File size:' + str(size_count)
-            status += '200: ' + str(count_200) + '\n'
-            status += '301: ' + str(count_200) + '\n'
-            status += '400: ' + str(count_200) + '\n'
-            status += '401: ' + str(count_200) + '\n'
-            status += '403: ' + str(count_200) + '\n'
-            status += '404: ' + str(count_200) + '\n'
-            status += '405: ' + str(count_200) + '\n'
-            status += '500: ' + str(count_200) + '\n'
-            if line_count == 10:
-                print(status)
-                sys.exit()
-except KeyboardInterrupt:
-    print(status)
+                if line_count == 10:
+                    print_status(status)
+    except KeyboardInterrupt:
+        print_status(status)
