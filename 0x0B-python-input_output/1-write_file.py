e259c8264f6c82256784a0cc92a0b35867ce9655
thomaskitaba <thomaskitaba@gmail.com>
2023-06-13 14:43:02 +0300

    printfile

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+    Write a function that writes
+    a string to a text file
+"""
+
+
+def write_file(filename="", text=""):
+    """
+    step-1 open the file in read mode
+    step-2 Read from the opend file
+    step-3 print it on the stdout
+    """
+    with open(filename, 'r') as fr:
+        with open(text, 'w') as fw:
+            fr_read = fr.read()
+            fw.write(fr_read)
+            return (len(fr_read))
