9655a7355f73a2a0e53ef80861ba6d52bb2fdb53
thomaskitaba <thomaskitaba@gmail.com>
2023-06-19 22:43:18 +0300

    update pycode style

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -30,12 +30,13 @@ class Rectangle(Base):
 
     @property
     def width(self):
-        """Set/get the width of the Rectangle."""
+        """ Getter for width """
         return self.__width
 
     @width.setter
     def width(self, value):
-        if type(value) != int:
+        """ Setter for width """
+        if not isinstance(value, int):
             raise TypeError("width must be an integer")
         if value <= 0:
             raise ValueError("width must be > 0")
@@ -43,12 +44,13 @@ class Rectangle(Base):
 
     @property
     def height(self):
-        """Set/get the height of the Rectangle."""
+        """ Getter for height """
         return self.__height
 
     @height.setter
     def height(self, value):
-        if type(value) != int:
+        """ Setter for height """
+        if not isinstance(value, int):
             raise TypeError("height must be an integer")
         if value <= 0:
             raise ValueError("height must be > 0")
