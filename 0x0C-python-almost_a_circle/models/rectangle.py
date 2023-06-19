bcbefb4d4b45e353c06b446329a081f366a87883
thomaskitaba <thomaskitaba@gmail.com>
2023-06-19 22:38:40 +0300

    update pycode style

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -167,7 +167,7 @@ class Rectangle(Base):
         }
 
     def __str__(self):
-         """ print instance of a class """
+        """Return the print() and str() representation of the Rectangle."""
         return "[Rectangle] ({}) {}/{} - {}/{}".format(self.id,
                                                        self.x, self.y,
                                                        self.width, self.height)
