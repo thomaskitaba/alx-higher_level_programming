44f59972fbefc8fbb0fa164b2b990c190cdc9692
thomaskitaba <thomaskitaba@gmail.com>
2023-06-19 20:01:21 +0300

    update base_test  save to file

--- 0x0C-python-almost_a_circle/tests/base_test.py
+++ 0x0C-python-almost_a_circle/tests/base_test.py
@@ -136,7 +136,6 @@ class test_from_json_string(unittest.TestCase):
         ---1--- edge cases: with out arg None, as list empty list
                 for Rectangle and Square
         ---2--- list of tupels, list of lists list of tuples
-        ---3---
     """
     def tearDown(self):
         try:
@@ -170,7 +169,7 @@ class test_from_json_string(unittest.TestCase):
         with open("Rectangle.json", 'w') as f:
             f = f.write(Base.to_json_string(br1.to_dictionary()))
         with open("Rectangle.json", 'r') as f:
-            self.assertEqual(Base.from_json_string(f.read())[square] == 2)
+            self.assertEqual(len(Base.from_json_string(f.read())), 5)
 
     #to Square
     def test_square_from_json_string_none(self):
@@ -187,25 +186,194 @@ class test_from_json_string(unittest.TestCase):
             self.assertTrue("[]" == f.read())
 
     def test_square_from_json_string_normal(self):
-        br1 = Square(2, 2, 2, 2)
-        with open("square.json", 'w') as f:
-            f = f.write(Base.to_json_string(br1.to_dictionary()))
-        with open("square.json", 'r') as f:
-            f.read
-            self.assertEqual(Base.from_json_string(f.read())[square] == 2) #, '{"id": 6, "Square": 2, "x": 2, "y": 2}')
+        s1 = Square(2, 2, 2)
+        with open("Square.json", 'w') as f:
+            f = f.write(Base.to_json_string(s1.to_dictionary()))
+        with open("Square.json", 'r') as f:
+            self.assertEqual(Base.from_json_string(f.read())["size"], 2) #, '{"id": 6, "Square": 2, "x": 2, "y": 2}')
 
 
 class test_create(unittest.TestCase):
-    """ 5- test create """
-    pass
+    """ TEST CREATE
+        ---1--- Edge cases: no arg, None, empty list, one
+        ---2--- Test invalid arguments list, tuple, set
+        ---3--- Test invalid date to width, height, x, y
+        ---4--- Test 1, 2, 3, 4
+    """
+
+    """---1--- Edge cases: no arg, None, empty list, one"""
+    def test_create_with_no_args(self):
+        r1 = Rectangle(2, 2, 2, 2)
+        r2 = Rectangle.create()
+        self.assertEqual(r1.id, r2.id - 1)
+
+    def test_create_with_none_as_id(self):
+        r1 = Rectangle(2, 2, 2, 2)
+        with self.assertRaises(TypeError):
+            r2 = Rectangle.create(None)
+
+    def test_create_with_none_as_id(self):
+        r1 = Rectangle(2, 2, 2, 2)
+        with self.assertRaises(TypeError):
+            r2 = Rectangle.create({})
+
+    # TODO:
+    """---2--- Test invalid arguments list, tuple, set"""
+
+    def test_create_with_list_kwarg(self):
+
+        my_dict = [{'height': 4, 'width': 10, 'id': 89}, {'height': 7, 'width': 1, 'id': 7}]
+        with self.assertRaises(TypeError):
+            r2 = Rectangle.create(**my_dict)
+
+    def test_create_with_tuple_kwarg(self):
+        my_dict = (2, 3, 4, 5)
+        with self.assertRaises(TypeError):
+            r2 = Rectangle.create(**my_dict)
+
+    def test_create_with_set_kwarg(self):
+        my_dict = {2, 4, 6}
+        with self.assertRaises(TypeError):
+            r2 = Rectangle.create(**my_dict)
+
+    def test_create_with_string(self):
+        my_dict = "string"
+        with self.assertRaises(TypeError):
+            r2 = Rectangle.create(**my_dict)
+
+    """---4--- test 1, 2, 3, 4, 5 arguments"""
+    def test_create_with_one_args(self):
+
+        my_dict = {'height': 4}
+        r2 = Rectangle.create(**my_dict)
+        with open("Rectangle.json", 'w') as f:
+            f = f.write(Base.to_json_string(r2.to_dictionary()))
+        with open("Rectangle.json", 'r') as f:
+            self.assertEqual(Base.from_json_string(f.read())["height"], 4)
+
+    def test_create_with_two_args(self):
+
+        my_dict = {'height': 4, 'width': 10}
+        r2 = Rectangle.create(**my_dict)
+        with open("Rectangle.json", 'w') as f:
+            f = f.write(Base.to_json_string(r2.to_dictionary()))
+        with open("Rectangle.json", 'r') as f:
+            self.assertEqual(Base.from_json_string(f.read())["height"], 4)
+
+    def test_create_with_three_args(self):
+
+        my_dict = {'height': 4, 'width': 10, 'id': 89}
+        r2 = Rectangle.create(**my_dict)
+        with open("Rectangle.json", 'w') as f:
+            f = f.write(Base.to_json_string(r2.to_dictionary()))
+        with open("Rectangle.json", 'r') as f:
+            self.assertEqual(Base.from_json_string(f.read())["height"], 4)
+
+    def test_create_with_four_args(self):
+
+        my_dict = {'height': 4, 'width': 10, 'id': 89, 'x': 2}
+        r2 = Rectangle.create(**my_dict)
+        with open("Rectangle.json", 'w') as f:
+            f = f.write(Base.to_json_string(r2.to_dictionary()))
+        with open("Rectangle.json", 'r') as f:
+            self.assertEqual(Base.from_json_string(f.read())["height"], 4)
+
+    def test_create_with_invalid_key_args(self):
+
+        my_dict = {'height': 4, 'width': 10, 'id': 89, 'x': 2, 'z': 3}
+        r2 = Rectangle.create(**my_dict)
+        with open("Rectangle.json", 'w') as f:
+            f = f.write(Base.to_json_string(r2.to_dictionary()))
+        with open("Rectangle.json", 'r') as f:
+           with self.assertRaises(KeyError):
+               Base.from_json_string(f.read())['']
+
+
 
 class test_load_from_file(unittest.TestCase):
-    """ 6- test load_from_file """
-    pass
+    """ test load_from_file
+        ---1--- load from single dict
+        ---2--- load from multiple dict and check last and first obj
+        ---3--- save invalid args
+    """
+    def tearDown(self):
+        try:
+            os.remove("Base.json")
+            os.remove("Rectangle.json")
+            os.remove("Square.json")
+        except Exception as e:
+            pass
+
+    """---1--- load from single dict"""
+    def test_load_from_file_single_obj(self):
+        r1 = Rectangle(2, 3, 4)
+        Rectangle.save_to_file([r1])
+        r2 = Rectangle.load_from_file()
+        self.assertEqual(str(r1), str(r2[0]))
+
+    """---2--- load from multiple dict and check last and first obj"""
+    def test_load_from_file_single_obj(self):
+        r1 = Rectangle(2, 3, 4)
+        r2 = Rectangle(5, 6, 7)
+        Rectangle.save_to_file([r1, r2])
+        r3 = Rectangle.load_from_file()
+        self.assertEqual(str(r2), str(r3[1]))
+
+    "---3--- save invalid args"
+    def test_for_invalid_arguments(self):
+        r1 = Rectangle(2, 3)
+        Rectangle.save_to_file([r1])
+        with self.assertRaises(TypeError):
+            Rectangle.load_from_file([])
+
 
 class test_save_to_file_csv(unittest.TestCase):
-    """ 7- test save_to_file_csv """
-    pass
+    """ test save_to_file_csv """
+    def tearDown(self):
+        try:
+            os.remove("Base.json")
+            os.remove("Rectangle.json")
+            os.remove("Square.json")
+        except Exception as e:
+            pass
+
+    @classmethod
+    def tearDown(self):
+        try:
+            os.remove("Base.json")
+            os.remove("Rectangle.json")
+            os.remove("Square.json")
+        except IOError:
+            pass
+
+    def test_rect_to_file_csv_one_arg(self):
+        r1 = Rectangle(10, 7, 2, 8)
+        list_rectangles_input = [r1]
+        Rectangle.save_to_file_csv(list_rectangles_input)
+
+        with open("Rectangle.csv", 'r') as f:
+            self.assertEqual(f"{r1.id},10,7,2,8\n", f.read())
+
+    def test_rect_to_csv_multiple_list(self):
+        r1 = Rectangle(10, 7, 2, 8)
+        r2 = Rectangle(2, 2, 2, 2)
+        list_rectangles_input = [r1, r2]
+        Rectangle.save_to_file_csv(list_rectangles_input)
+
+        with open("Rectangle.csv", 'r') as f:
+            self.assertEqual(f"{r1.id},10,7,2,8\n{r2.id},2,2,2,2\n", f.read())
+
+    def test_square_to_csv_string_type(self):
+        bs1 = Square(2, 2, 2)
+        self.assertTrue(Base.to_json_string(bs1.to_dictionary()), str)
+
+    def test_square_to_csv_string_multiple_list(self):
+        bs1 = Square(2, 2, 2)
+        bs2 = Square(3, 3, 3)
+        list_dictionary = [bs1.to_dictionary(), bs2.to_dictionary()]
+        self.assertEqual(len(Base.to_json_string(list_dictionary)), 78)
+
+
 
 class test_load_from_file_csv(unittest.TestCase):
     """ 8- test load_from_file_csv """
